61736e208e7d991fe9fff41719dcefd530242b10
avivdolan11 <aviv.dolan@sportcast.com.au>
2024-08-23 05:45:04 +0000

    print multiple args

---
+++ 02-Welcome-to-Python/the-print-function-II-multiple-arguments.py
@@ -0,0 +1,8 @@
+print("a", "b")
+print("c", "d", "e", "f")
+
+print(5+3, 2-8)
+print(5+3, 2-8)
+print(5+3, 2-8)  ### shift + alt + down reprints line above
+
+print("Hello", 10, 3.14)
---
+++ 02-Welcome-to-Python/the-print-function-III-parameters-and-arguments.py
@@ -0,0 +1,9 @@
+print("ABC", "DEF", "XYZ")
+print("ABC", "DEF", "XYZ", sep="!")
+
+
+print("Hello", end="#*@")
+print("Hello")
+
+print("A", "B", "C", sep="**", end="#")
+print("A", "B", "C", end="#", sep="**")
