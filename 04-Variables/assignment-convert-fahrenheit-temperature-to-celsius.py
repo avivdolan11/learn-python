adb08953394152f2e5e3e2892ededebedfa3367c
avivdolan11 <aviv.dolan@sportcast.com.au>
2024-09-29 10:44:52 +0000

    variables

---
+++ 04-Variables/assignment-convert-fahrenheit-temperature-to-celsius.py
@@ -0,0 +1,3 @@
+temp_far = float(input("What is the temperature in farenheit? "))
+print((temp_far - 32)*(5/9),  "Celsius")
+
---
+++ 04-Variables/multiple-variable-assignments.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+
+a = b = 5
+
+b = 10
+print(b)
+print(a)
+
+a, b = 5, 10
+print(a)
+print(b)
